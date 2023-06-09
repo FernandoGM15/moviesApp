c80c3b780794dd04dac0a80d00bec63a75f92898
Fernando <lgarcia@tredicom.com>
2023-06-09 10:11:45 -0600

    fix: :bug: Added a uuid as id on store user

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,42 +1,43 @@
-import { URL } from "../environment"
+import { URL } from "../environment";
+import { v4 as uuid } from "uuid";
 
 export interface UserI {
-    id: number;
-    name: string;
-    email: string;
+  id: number;
+  name: string;
+  email: string;
 }
 
-type UserParamsI = Partial<Record<keyof UserI, string>>
-
-
+type UserParamsI = Partial<Record<keyof UserI, string>>;
 
 /**
- * @description Function to fetches users from the API 
+ * @description Function to fetches users from the API
  */
 export const getUsers = async (params?: UserParamsI): Promise<UserI[]> => {
-    const query = params ? new URLSearchParams(params).toString() : '';
-    const req = await fetch(`${URL}/users?${query}`)
-    if (req.ok) {
-        return req.json()
-    }
-    throw new Error("Error fetching users")
-}
-
+  const query = params ? new URLSearchParams(params).toString() : "";
+  const req = await fetch(`${URL}/users?${query}`);
+  if (req.ok) {
+    return req.json();
+  }
+  throw new Error("Error fetching users");
+};
 
 /**
  * @description  Function to store a user in the API
  */
-export const storeUser = async (formElement: HTMLFormElement): Promise<UserI> => {
-    const form = new FormData(formElement)
-    const req = await fetch(`${URL}/users`, {
-        body: JSON.stringify(Object.fromEntries(form)),
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    if (req.ok) {
-        return req.json()
-    }
-    throw new Error("Error storing user")
-}
+export const storeUser = async (
+  formElement: HTMLFormElement
+): Promise<UserI> => {
+  const form = new FormData(formElement);
+  form.append("id", uuid());
+  const req = await fetch(`${URL}/users`, {
+    body: JSON.stringify(Object.fromEntries(form)),
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (req.ok) {
+    return req.json();
+  }
+  throw new Error("Error storing user");
+};
